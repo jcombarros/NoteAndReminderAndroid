a95c93dedf818c3b850e72dbfde5967fb38a7c6b
Javier Combarros <Javier@138.4.192.251>
2015-06-06 16:15:15 +0200

    Login page layout definition

--- src/es/jab/noteandreminderandroid/activity/MainActivity.java
+++ src/es/jab/noteandreminderandroid/activity/MainActivity.java
@@ -1,13 +1,14 @@
 package es.jab.noteandreminderandroid.activity;
 
 import es.jab.noteandreminderandroid.R;
-import es.jab.noteandreminderandroid.R.id;
-import es.jab.noteandreminderandroid.R.layout;
-import es.jab.noteandreminderandroid.R.menu;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -16,8 +17,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button boton = (Button) findViewById(R.id.LoginButtonHome);
+		boton.setOnClickListener(new OnClickListener(){
+        	public void onClick(View v){
+        		click(v);
+        	}
+        });
     }
 
+
+	private void click(View v) {
+		Intent intent = new Intent(v.getContext(), LoginActivity.class);
+    	intent.putExtra("message", "From MainActivity");
+    	startActivityForResult(intent, 0);
+		
+	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
