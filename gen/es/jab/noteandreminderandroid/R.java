3746901d8b3d0271e75de19d7c5c7f7463265c63
Javier Combarros <Javier@138.4.192.251>
2015-06-07 20:14:29 +0200

    View Reminder page added. Activity and layout.

--- gen/es/jab/noteandreminderandroid/R.java
+++ gen/es/jab/noteandreminderandroid/R.java
@@ -83,15 +83,17 @@ public final class R {
         public static final int activity_note=0x7f030002;
         public static final int activity_notes=0x7f030003;
         public static final int activity_notes_note=0x7f030004;
-        public static final int activity_reminders=0x7f030005;
-        public static final int activity_reminders_reminder=0x7f030006;
+        public static final int activity_reminder=0x7f030005;
+        public static final int activity_reminders=0x7f030006;
+        public static final int activity_reminders_reminder=0x7f030007;
     }
     public static final class menu {
         public static final int login=0x7f080000;
         public static final int main=0x7f080001;
         public static final int note=0x7f080002;
         public static final int notes=0x7f080003;
-        public static final int reminders=0x7f080004;
+        public static final int reminder=0x7f080004;
+        public static final int reminders=0x7f080005;
     }
     public static final class string {
         public static final int Description1HomeString=0x7f060005;
@@ -125,6 +127,7 @@ public final class R {
         public static final int title_activity_login=0x7f06000b;
         public static final int title_activity_note=0x7f060015;
         public static final int title_activity_notes=0x7f060013;
+        public static final int title_activity_reminder=0x7f060020;
         public static final int title_activity_reminders=0x7f06001c;
     }
     public static final class style {
